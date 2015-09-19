bff7d5908f7d05632f06d3ce3537f24f7375654e
lentebloem <thisisreallyaname@gmail.com>
2015-09-19 16:59:45 -0700

    add progress bar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var dtd = angular.module('d2d', []);
+var dtd = angular.module('d2d', ['ngAnimate', 'ui.bootstrap']);
 
 dtd.controller("mainCtrl", ['$scope', '$location', '$http', function($scope, $location, $http){
   $scope.projects = [{
@@ -8,7 +8,8 @@ dtd.controller("mainCtrl", ['$scope', '$location', '$http', function($scope, $lo
     'focus': 'Health Kits Focus',
     'breakdown':'$280 to equip 1 Community Health Worker with essential medication and supplies.  71%: Essential medical and logistical supplies including e.g. soap, stationery (counseling cards and job aids), a thermometer, respiratory timer, water jugs and a backpack. 29%: Critical medication including medicines to treat diarrhea, malaria, pneumonia, malnutrition and care for pregnant mothers.',
     'cashtag': 'fear',
-    'type':
+    'type': 'success',
+    'value': '40'
   },{
     'amount': '$313 more needed',
     'location': 'Hargeisa, Somaliland ',
@@ -16,7 +17,9 @@ dtd.controller("mainCtrl", ['$scope', '$location', '$http', function($scope, $lo
     'focus': 'Birth Injuries Focus',
     'breakdown':'$425 per treatment 47%: Operation. 19%: Medication &amp; dressings. 19%: Hospital stay (8 days). 9%: Lab tests, screening, grouping &amp; X-matching. 6%: Anesthesia.',
     'treatment_description': 'Our focus is on providing safe births and caesarean sections for mothers. In Somaliland most mothers do not receive prenatal care. As a result, they often have unknown problems such as anemia, diabetes or hypertension that go untreated. Sometimes more serious issues develop, such as obstructed labor; in these cases, we do a surgical procedure known as a caesarian section, which involves making an incision through the mother\'s abdomen to deliver the baby.',
-    'cashtag': 'fear'
+    'cashtag': 'fear',
+    'type': 'success',
+    'value': '50'
   }
   ];
 

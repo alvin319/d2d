e1ff0f698dc63e44a5d1e3a1d42e06f9d189c022
mykobob <michael.eagle@utexas.edu>
2015-09-20 02:23:39 -0400

    added submit donation page

---
+++ submitDonation.js
@@ -0,0 +1,14 @@
+$(function() {
+    
+    var $formRegister = $('#register-form');
+    $('form').submit(function() {
+        var $
+
+        $('#register_login_btn').click( 
+            function () {
+                modalAnimate($formRegister, $formLogin); 
+            }
+        );
+    });
+
+});

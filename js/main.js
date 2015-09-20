4cb97876bdd1b57461a4d20d0616fe900b2b0fb6
lentebloem <thisisreallyaname@gmail.com>
2015-09-19 17:51:12 -0700

    add progress bar

--- js/main.js
+++ js/main.js
@@ -2,16 +2,28 @@ var dtd = angular.module('d2d', ['ngAnimate', 'ui.bootstrap']);
 
 dtd.controller("mainCtrl", ['$scope', '$location', '$http', function($scope, $location, $http){
   $scope.projects = [{
-    'amount': '$280 more needed',
+    'amountNeeded': 280,
+    'amountSofar': 0,
     'location': 'Ashanti, Ghana',
+    'money_description':'$280 to equip 1 Community Health Worker with essential medication and supplies. ',
     'treatment_description': 'Community Health Workers will be equipped with essential medication and supplies enabling them to treat women and children with critical illnesses including malaria, diarrhea, malnutrition, and pneumonia. These items make the difference between leading a productive life or being held back with increasingly serious medical conditions.',
     'focus': 'Health Kits Focus',
-    'breakdown':'$280 to equip 1 Community Health Worker with essential medication and supplies.  71%: Essential medical and logistical supplies including e.g. soap, stationery (counseling cards and job aids), a thermometer, respiratory timer, water jugs and a backpack. 29%: Critical medication including medicines to treat diarrhea, malaria, pneumonia, malnutrition and care for pregnant mothers.',
+    'breakdown': [
+      {'value': 71,
+      'type': 'danger',
+      'description': 'Essential medical and logistical supplies including e.g. soap, stationery (counseling cards and job aids), a thermometer, respiratory timer, water jugs and a backpack.'
+    },{
+        'value': 29,
+        'type': 'success',
+        'description':'Critical medication including medicines to treat diarrhea, malaria, pneumonia, malnutrition and care for pregnant mothers.'
+      }
+    ],
     'cashtag': 'fear',
     'type': 'success',
     'value': '40'
   },{
-    'amount': '$313 more needed',
+    'amountNeeded': 425,
+    'amountSofar': 313,
     'location': 'Hargeisa, Somaliland ',
     'description': 'My name is Dr. Shukri Mohamed Dahir and sitting next to me is Dr. Naima Ahmed Mohamed. We are both doctors: we went to Medical School together and are now working as a &hellip;',
     'focus': 'Birth Injuries Focus',
@@ -23,4 +35,12 @@ dtd.controller("mainCtrl", ['$scope', '$location', '$http', function($scope, $lo
   }
   ];
 
+  $scope.updateProgressBar = function() {
+    var types = ['danger','warning','info','success'];
+    for (project in $scope.projects) {
+      project.value = Math.floor(project.amountSofar / project.amountNeeded * 100);
+      project.type = types[Math.floor(project.value/25)];
+    }
+  };
+  $scope.updateProgressBar();
 }]);
